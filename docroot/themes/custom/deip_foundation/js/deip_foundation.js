a0b033a536e6ca71ffcabcde3664924352b8fd9f
mariagwyn <mariagwyn@gmail.com>
2016-12-14 11:51:53 -0800

    MGM-28: Code cleanup via phpcbf.

--- docroot/themes/custom/deip_foundation/js/deip_foundation.js
+++ docroot/themes/custom/deip_foundation/js/deip_foundation.js
@@ -1,17 +1,16 @@
 /**
  * @file
  * Placeholder file for custom sub-theme behaviors.
- *
  */
 (function ($, Drupal) {
 
-  /**
+    /**
    * Use this behavior as a template for custom Javascript.
    */
-  Drupal.behaviors.exampleBehavior = {
-    attach: function (context, settings) {
-      //alert("I'm alive!");
-    }
-  };
+    Drupal.behaviors.exampleBehavior = {
+        attach: function (context, settings) {
+            //alert("I'm alive!");
+        }
+    };
 
 })(jQuery, Drupal);
