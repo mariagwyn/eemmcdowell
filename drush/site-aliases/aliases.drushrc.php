2d5eeb295492b40ad4d7a35198398cc0a4f63331
mariagwyn <mariagwyn@gmail.com>
2017-01-23 16:21:43 -0800

    MGM-37 | Updating travis ci.

--- drush/site-aliases/aliases.drushrc.php
+++ drush/site-aliases/aliases.drushrc.php
@@ -358,4 +358,14 @@
 #$aliases['live'] = array(
 #    'uri' => 'http://example.com',
 #    'root' => $aliases['dev']['root'],
-#  ) + $aliases['server'];
+#  ) + $aliases['server'];// [vagrant_machine_name].local
+$aliases['eemmcdowell.local'] = array(
+  // /var/www/[vagrant_machine_name]/docroot
+  'root' => '/var/www/eemmcdowell/docroot',
+  // vagrant_hostname
+  'uri' => 'http://local.eemmcdowell.dd:8083',
+  // vagrant_hostname
+  'remote-host' => 'local.eemmcdowell.dd:8083',
+  'remote-user' => 'vagrant',
+  'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
+);
